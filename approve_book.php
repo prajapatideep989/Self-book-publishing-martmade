746f63b33e40cb9a0528f7d0484176b092ef3a4e
prajapatideep989 <prajapatideep081@gmail.com>
2026-04-28 10:45:50 +0530

    Removed SMTP credentials

--- approve_book.php
+++ approve_book.php
@@ -41,8 +41,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'deepprajapati1012@gmail.com';
-            $mail->Password = 'ybfv bmrc rjno bvkv';
+            $mail->Username = '';
+            $mail->Password = '';
             $mail->SMTPSecure = 'tls';
             $mail->Port = 587;
 
